6be6bb1b7eef4d140f6f7737534340da3250b47e
Ítalo Lima <italojonas@hotmail.com>
2021-04-18 12:02:23 -0300

    integration faunadb

--- src/pages/api/auth/[...nextauth].ts
+++ src/pages/api/auth/[...nextauth].ts
@@ -1,5 +1,8 @@
+import { query } from 'faunadb'
 import NextAuth from 'next-auth'
 import Providers from 'next-auth/providers'
+
+import { fauna } from "../../../services/fauna"
 
 export default NextAuth({
   providers: [
@@ -9,4 +12,38 @@ export default NextAuth({
       scope: 'read:user'
     }),
   ],
+  callbacks: {
+    async signIn(user, account, profile) {
+      const { email, } = user
+
+      try {
+        await fauna.query(
+          query.If(
+            query.Not(
+              query.Exists(
+                query.Match(
+                  query.Index('user_by_email'),
+                  query.Casefold(email)
+                )
+              )
+            ),
+            query.Create(
+              query.Collection('users'),
+              {data: {email}}
+            ),
+            query.Get(
+              query.Match(
+                query.Index('user_by_email'),
+                query.Casefold(email)
+              )
+            )
+          )
+        )
+
+        return true
+      } catch {
+        return false
+      }
+    }
+  }
 })
